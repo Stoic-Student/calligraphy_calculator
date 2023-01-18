f642429fb40b9a16db20a970444dcdf88f97b7d7
Dagmar-LiveHelfi <dagmar.biomedvisuals@gmail.com>
2023-01-18 19:57:00 +0100

    Klaar met berekening herschrijven (?)

--- app.js
+++ app.js
@@ -88,38 +88,6 @@ async function verwerkFormulierInformatie() {
   maakKarakterMap(berekeningObject.penNib.strokeBreedte, berekeningObject.woordafstand)
 
   outputInfoOpstellen(berekeningObject, karakterMap)
-
-  // let tekstlengte = 0;
-  // let karaktersInZinArray = [];
-  // let karakterLengteArray  = [];
-  // let karakterStartCoordinatenArray = [];
-
-  // // Bereken de outputs voor de ingevulde tekst
-  // for (let i = 0; i < berekeningObject.tekst.length; i++) {
-
-  //   // Tijdelijke opslag lengte van karakter op plaats i
-  //   let lengteVanKarakter = karakterMap.get(berekeningObject.tekst.charAt(i));
-
-  //   // Vul startcoordinaten in bij output tabel arrays
-  //   karakterStartCoordinatenArray.push(tekstlengte)
-  //   // Voeg lengte van karakter toe aan tekstlengte
-  //   tekstlengte += lengteVanKarakter
-    
-  //   // Voeg ruimte tussen karakters toe aan tekstlengte tot laatste karakter
-  //   if ((i+1) < berekeningObject.tekst.length) {
-  //     tekstlengte += berekeningObject.letterafstand
-  //   }
-
-  //   // Vul karakter en karakterlengte in bij output tabel arrays
-  //   karaktersInZinArray.push(berekeningObject.tekst[i])
-  //   karakterLengteArray.push(lengteVanKarakter)
-    
-  //   // console.log(berekeningObject.tekst[i] +" = "+ lengteVanKarakter +" > totale lengte = "+ tekstlengte)
-  // }
-  // berekeningObject.tekstlengte = tekstlengte
-  // berekeningObject.karakterArray = karaktersInZinArray
-  // berekeningObject.karakterLengteArray = karakterLengteArray
-  // berekeningObject.karakterStartCoordinatenArray = karakterStartCoordinatenArray
 }
 
 // ----------------------
